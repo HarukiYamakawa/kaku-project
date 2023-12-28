73ca9116413bc146de73dbd1e7d22ce40bf3dc56
HarukiYamakawa <visuhalkey@gmail.com>
2023-12-29 02:18:57 +0900

    商品データを取得するコントローラを実装

--- kaku-backend/db/seeds.rb
+++ kaku-backend/db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Productsのシードデータ
+products = [
+  { name: "蜜柑", price: 500, description: "果物です", image_url: "#" },
+  { name: "餅", price: 600, description: "穀物です", image_url: "#" },
+  { name: "ナッツ", price: 700, description: "種実です", image_url: "#" },
+  { name: "栄螺", price: 800, description: "貝類です", image_url: "#" },
+]
+
+# データベースに追加
+products.each do |product|
+  Product.create!(product)
+end
