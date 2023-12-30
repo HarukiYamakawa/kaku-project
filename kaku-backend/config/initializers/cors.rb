86b1e7ee780bb7d68fd34facade42f0456d919d8
HarukiYamakawa <visuhalkey@gmail.com>
2023-12-30 23:10:23 +0900

    Nginxのセットアップ

--- kaku-backend/config/initializers/cors.rb
+++ kaku-backend/config/initializers/cors.rb
@@ -1,10 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Settings.front_domain
+    origins Settings.front_domain, Settings.front_domain_next
 
     resource "*",
       headers: :any,
-      expose: ['access-token', 'uid','client'],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
